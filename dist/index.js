5f564b72f7baf01747ca9a08e1fbaacfbc330d5e
snowfirewolf <4399kevin11@gmail.com>
2021-08-17 18:28:17 +0800

    for deno

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.motdParser = void 0;
-var motdParser_1 = require("./motdParser");
-Object.defineProperty(exports, "motdParser", { enumerable: true, get: function () { return motdParser_1.motdParser; } });
--- dist/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*
- * minecraft motd parser v1.0.0
- * (c) 2021 Kevin Zheng
- * Released under the MIT license
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-;
-;
