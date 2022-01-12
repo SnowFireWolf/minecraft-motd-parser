e8055a84126c86c2fb55c1820097437b5020d6b7
SnowFireWolf <4399kevin11@gmail.com>
2022-01-13 01:55:53 +0800

    hot fix dist type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,10 +2,10 @@ import { motdJsonType } from './types';
 declare function cleanTags(text: string): string;
 declare function textToHTML(motdString: string): string;
 declare function parseTextToJSON(text: string): motdJsonType;
-declare function parseJSONToHTML(sourceJson: motdJsonType): string;
-declare function jsonEnterRender(json: motdJsonType): string;
+declare function parseJSONToHTML(sourceJson: motdJsonType | object): string;
+declare function jsonEnterRender(json: motdJsonType | object): string;
 declare function textEnterRender(text: string): string;
-declare function autoToHtml(motd: motdJsonType | string): string;
+declare function autoToHtml(motd: motdJsonType | string | object): string;
 export declare let motdParser: {
     cleanTags: typeof cleanTags;
     textToHTML: typeof textToHTML;
