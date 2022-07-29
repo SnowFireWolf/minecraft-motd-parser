4a0142e58c9bbc9301389631a1a1e2cf1ed2df77
SnowFireWolf <4399kevin11@gmail.com>
2022-07-29 22:46:11 +0800

    released 1.0.9

--- dist/types.js
+++ dist/types.js
@@ -1,9 +1,1 @@
-"use strict";
-/*
- * minecraft motd parser v1.0.9
- * (c) 2022 Kevin Zheng
- * Released under the MIT license
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-;
-;
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0})
